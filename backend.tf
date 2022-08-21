aba7ff3a9dfd4bb92efa5225ee956ea5f0e687c0
Oscar Lee <leftwing871@gmail.com>
2022-08-22 06:17:37 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" { # 강의는 
-      bucket         = "terraform-s3-bucket-ehlee" # s3 bucket 이름
+    backend "s3" { 
+      bucket         = "terraform-s3-bucket-demo" # s3 bucket 이름
       key            = "terraform/own-your-path/terraform.tfstate" # s3 내에서 저장되는 경로를 의미합니다.
       region         = "ap-northeast-2"  
       encrypt        = false
-      dynamodb_table = "terraform-ehlee-lock"
+      dynamodb_table = "terraform-demo-lock" ##DDB table 이름
     }
 }
