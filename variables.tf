486529caa4e40f4249950b8efe6ad02f9bdbb408
Oscar Lee <leftwing871@gmail.com>
2022-08-22 06:19:14 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 # EC2
 
+variable "vpc_id" {
+    type = string
+    description = "VPC id"
+}
+
 variable "key" {
     type = string
     description = "EC2 Key pairs name"
@@ -9,4 +14,11 @@ variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
   default     = "ExampleAppServerInstance"
+}
+
+
+variable "ami_id" {
+    type = string
+    default = "ami-01711d925a1e4cc3a"
+    description = "ami id"
 }
