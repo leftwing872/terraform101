9673ffceba538b9f9e5c21b150c136755a2b697b
eunhak.github <leftwing871@gmail.com>
2023-11-07 09:17:27 +0900

    add module

--- tfworkshop/helloworld/main.tf
+++ tfworkshop/helloworld/main.tf
@@ -62,3 +62,26 @@ resource "aws_security_group_rule" "app_server_sg_ingress1" {
     security_group_id = aws_security_group.app_server_sg.id
     description = "Ingress app_server ssh"
 }
+
+module "ec2_instance" {
+  source = "./modules/ec2"
+  region = "ap-northeast-2"
+  ami_id ="ami-0e01e66dacaf1454d"
+  instance_type = "t3.micro"
+  key_name = "YOUR_KEY_PAIR"
+  instance_count = 1
+  subnet_ids = ["subnet-071de07b184a6f908", "subnet-0894f2b01c7fc7b6f"]
+  tags = local.tags_merge
+  name = "test"
+  environment = "dev"
+  owner = "yourname"
+  cost_center = "rnd"
+  application = "example_frontend"
+  security_group_ids = [aws_security_group.app_server_sg.id]
+}
+
+
+# module "consul" {
+# 	source = "app.terraform.io/example-corp/k8s-cluster/azurerm"
+#    	version = "1.1.0"
+# }
--- tfworkshop/helloworld/outputs.tf
+++ tfworkshop/helloworld/outputs.tf
@@ -1,3 +1,9 @@
 output "public_ip" {
     value = aws_instance.app_server[*].public_ip
 }
+
+output "instance_public_ip_by_module" {
+  description = "Public IP address of the EC2 instance by module"
+  value       = module.ec2_instance.instance_public_ip
+}
+
